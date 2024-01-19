4282383a0ac83295b0b075fbaaa06f91e0e35797
than2042 <than.than2042@gmail.com>
2024-01-19 17:26:58 +0000

    add delete & search function

--- client/src/pages/post/Post.jsx
+++ client/src/pages/post/Post.jsx
@@ -1,30 +1,53 @@
 import { useState, useEffect } from "react";
-import { TextField } from "@mui/material";
-
+import { TextField, Button } from "@mui/material";
 import "./Post.css";
 
 const Post = () => {
   const APIURL = "http://localhost:8080/posts";
   const [getPost, setGetPost] = useState([]);
   const [search, setSearch] = useState("");
+  const [deletePost, setDeletePost] = useState(null);
 
   useEffect(() => {
     handleGetPost();
-  }, []);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [search, deletePost]);
 
   const handleGetPost = async () => {
-    const response = await fetch(`${APIURL}?search=${search}`);
+    const response = await fetch(`${APIURL}`);
     const data = await response.json();
     setGetPost(data);
   };
 
-  const handleSearch = () => {
-    const filteredPosts = getPost.filter((post) => {
-      post.title.toLowerCase().includes(search.toLowerCase()) ||
-        post.content.toLowerCase().includes(search.toLowerCase());
+  const handleSearch = async (e) => {
+    let searchItem = e.target.value.toLowerCase();
+    setSearch(searchItem);
+
+    try {
+      const response = await fetch(`${APIURL}?search=${searchItem}`);
+      const data = await response.json();
+      const filteredPosts = data.filter((post) =>
+        post.title.toLowerCase().includes(search.toLowerCase())
+      );
+      console.log(filteredPosts, "fi");
+      setGetPost(filteredPosts);
+    } catch (error) {
+      console.error("Error fetching posts:", error);
+    }
+  };
+
+  const handleDelete = async (postId) => {
+    const response = await fetch(`${APIURL}/${postId}`, {
+      method: "DELETE",
     });
-    console.log(filteredPosts, "se");
-    setSearch(filteredPosts);
+    console.log(response, "res");
+
+    if (response.ok) {
+      console.log("Post deleted successfully");
+      setDeletePost(postId);
+    } else {
+      console.error("Error deleting", response.status);
+    }
   };
 
   return (
@@ -35,21 +58,37 @@ const Post = () => {
           placeholder="Search"
           id="filled-hidden-label-normal"
           variant="filled"
-          onChange={(e) => setSearch(e.target.value)}
+          onChange={handleSearch}
           value={search}
-          onClick={handleSearch}
         />
       </div>
-      <div className="postItem">
-        {getPost.map((post) => {
-          return (
-            <div className="card" key={post.id}>
-              <h3 key={post.title}>{post.title}</h3>
-              <p key={post.content}>{post.content}</p>
-              <p key={post.createdat}>{post.createdat}</p>
-            </div>
-          );
-        })}
+      <div className="itemContainer">
+        <div className="postItem">
+          {getPost.map((post) => {
+            return (
+              <div className="card" key={post.id}>
+                <h3 className="title" key={post.title}>
+                  {post.title}
+                </h3>
+                <p className="content" key={post.content}>
+                  {post.content}
+                </p>
+                <p className="time" key={post.createdat}>
+                  {post.createdat}
+                </p>
+                {post.image && <img src={post.image} alt={post.title} />}
+                <Button
+                  id="deleteBtn"
+                  onClick={() => handleDelete(post.id)}
+                  variant="outlined"
+                  color="error"
+                >
+                  Delete Post
+                </Button>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import pg from "pg";
 import dotenv from "dotenv";
+import multer from "multer";
 
 dotenv.config();
 
@@ -9,6 +10,8 @@ const app = express();
 const PORT = 8080;
 app.use(express.json());
 app.use(cors());
+
+const upload = multer({ storage: multer.memoryStorage() });
 
 const dbConnectionString = process.env.DATABASE_URL;
 const db = new pg.Pool({ connectionString: dbConnectionString });
@@ -34,8 +37,22 @@ app.post("/users", async (req, res) => {
 });
 
 app.get("/posts", async (req, res) => {
-  const result = await db.query("SELECT * FROM posts");
+  const { search } = req.query;
+
+  let query = "SELECT * FROM posts";
+
+  if (search) {
+    query += "WHERE title ILIKE $1 id = $1";
+  }
+  const params = search ? [`%${search}%`] : [];
+  const result = await db.query(query, params);
   res.json(result.rows);
+});
+
+app.delete("/posts/:id", async (req, res) => {
+  const postId = req.params.id;
+  const result = await db.query("DELETE FROM posts WHERE id = $1", [postId]);
+  res.json(result.rowCount);
 });
 
 app.get("/posts/:userid", async (req, res) => {
@@ -46,20 +63,22 @@ app.get("/posts/:userid", async (req, res) => {
 app.get("/posts/:postsid", async (req, res) => {
   const postId = req.params.postsid;
   console.log("id", postId);
-  const result = await db.query("SELECT * FROM posts WHERE posts.id = $1", [
+  const result = await db.query("SELECT * FROM posts WHERE post.id = $1", [
     postId,
   ]);
   console.log("re", result.rows);
   res.json(result.rows);
 });
 
-app.post("/posts", async (req, res) => {
+app.post("/posts", upload.single("image"), async (req, res) => {
   const { title, content } = req.body;
-
+  const image = req.file ? req.file.buffer.toString("base64") : null;
+  console.log(image);
   const newPosts = db.query(
-    `INSERT INTO posts (title, content) VALUES ($1, $2)`,
-    [title, content]
+    `INSERT INTO posts (title, content, image) VALUES ($1, $2, $3)`,
+    [title, content, image]
   );
+
   res.json(newPosts);
 });
 
